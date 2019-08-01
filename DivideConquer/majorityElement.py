58830b19b807bbfca9c0d8a3cd3a3b13892ea23a
nbhushan@adobe.com <nidhi.bhushan123@gmail.com>
2019-08-01 22:24:50 +0530

    some more

---
+++ DivideConquer/majorityElement.py
@@ -0,0 +1,50 @@
+'''
+Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
+
+You may assume that the array is non-empty and the majority element always exist in the array.
+'''
+
+def majorityElement(nums, lo=0, hi=None):
+    def majority_element_rec(lo, hi):
+        
+        # base case; the only element in an array of size 1 is the majority
+        # element.
+        if lo == hi:
+            return nums[lo]
+
+        # recurse on left and right halves of this slice.
+        mid = (hi-lo)//2 + lo
+        v_l = nums[lo:mid+1]
+        v_r = nums[mid+1:hi+1]
+        left = majority_element_rec(lo, mid)
+        right = majority_element_rec(mid+1, hi)
+
+        # if the two halves agree on the majority element, return it.
+        if left == right:
+            return left
+
+        # otherwise, count each element and return the "winner".
+        left_count = sum(1 for i in range(lo, hi+1) if nums[i] == left)
+        right_count = sum(1 for i in range(lo, hi+1) if nums[i] == right)
+
+        return left if left_count > right_count else right
+
+    return majority_element_rec(0, len(nums)-1)
+
+
+
+def majorityElement_1(nums):
+    import collections
+    import sys
+
+    n = len(nums)//2
+    d = collections.Counter(nums)
+    max = -sys.maxsize
+    for k,v in d.items():
+        if v > max:
+            max = v
+            key = k
+    return key
+
+nums = [2,2,1,1,1,2,2]
+print(majorityElement(nums))
---
+++ DivideConquer/maxSubArray.py
@@ -0,0 +1,25 @@
+'''
+Given an eger array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+
+Example:
+
+Input: [9,1,-3,4,-1,2,1,-5,4],
+Output: 6
+Explanation: [4,-1,2,1] has the largest sum = 6.
+'''
+
+def maxSubArray(nums):
+    if (nums == None or len(nums) == 0):
+        return 0
+    maximum = nums[0]
+    sum = nums[0]
+    for i in range(1,len(nums)):
+        if sum < 0:
+            sum = nums[i]
+        else:
+            sum += nums[i]
+        maximum = max(maximum, sum)
+    return maximum
+
+nums = [9,1,-3,4,-1,12,1,-5,4]
+print(maxSubArray(nums))
