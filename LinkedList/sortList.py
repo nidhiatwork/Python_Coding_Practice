592c378439c25b3b3e493ed8cdcc1b577ebc44cf
nbhushan@adobe.com <nidhi.bhushan123@gmail.com>
2020-05-14 12:50:50 +0530

    linkedlist practice done

--- LinkedList/sortList.py
+++ LinkedList/sortList.py
@@ -13,31 +13,42 @@ class ListNode:
         self.val = val
         self.next = next
 
-def merge(h1, h2):
-        dummy = tail = ListNode(None)
-        while h1 and h2:
-            if h1.val < h2.val:
-                tail.next, tail, h1 = h1, h1, h1.next
-            else:
-                tail.next, tail, h2 = h2, h2, h2.next
-    
-        tail.next = h1 or h2
-        return dummy.next
-    
 def sortList(head):
     if not head or not head.next:
-        print("returning: ", head.val)
         return head
-        
-
-    pre, slow, fast = None, head, head
+    # divide list into two parts
+    fast, slow = head.next, head
     while fast and fast.next:
-        pre, slow, fast = slow, slow.next, fast.next.next
-    pre.next = None
-    print("Setting {} to None".format(pre.val))
-    a = sortList(head)
-    b = sortList(slow)
-    return merge(a, b)
+        fast = fast.next.next
+        slow = slow.next
+    second = slow.next
+    # cut down the first part
+    slow.next = None
+    h1 = sortList(head)
+    h2 = sortList(second)
+    return merge(h1, h2)
+
+def merge(h1, h2):
+    if not h1 or not h2:
+        return h1 or h2
+    if h1.val > h2.val:
+        h1, h2 = h2, h1
+    # get the return node "head"
+    head = pre = h1
+    h1 = h1.next
+    while h1 and h2:
+        if h1.val < h2.val:
+            h1 = h1.next
+        else:
+            nxt = pre.next
+            pre.next = h2
+            tmp = h2.next
+            h2.next = nxt
+            h2 = tmp
+        pre = pre.next
+    # h1 and h2 at least one is None
+    pre.next = h1 or h2
+    return head
 
 head = ListNode(4,ListNode(2,ListNode(1,ListNode(3,ListNode(9)))))
 head = sortList(head)
--- LinkedList/swap_nodes.py
+++ LinkedList/swap_nodes.py
@@ -3,7 +3,7 @@ Given a linked list, swap every two adjacent nodes and return its head.
 
 You may not modify the values in the list's nodes, only nodes itself may be changed.
 
-Given 1->2->3->4, you should return the list as 2->1->4->3.
+Given 1.2.3.4, you should return the list as 2.1.4.3.
 """
 
 class ListNode:
@@ -11,27 +11,39 @@ class ListNode:
         self.val = val
         self.next = next
 
-def swap_nodes(head):
+def swap_nodes_by_value(head):
+    temp = head 
+         
+    # There are no nodes in ilnked list 
+    if temp is None: 
+        return 
+        
+    # Traverse furthur only if there are at least two 
+    # left 
+    while(temp is not None and temp.next is not None): 
+
+        # Swap val of node with its next node's val 
+        temp.val, temp.next.val = temp.next.val, temp.val  
+            
+        # Move temo by 2 fro the next pair 
+        temp = temp.next.next
+
+def swap_nodes_by_links(head):
     if not head or not head.next:
         return head 
-
-    node = head.next
-    
-    curr = head.next  
-    prev = head
-    while True:
-            next = curr.next
-            curr.next = prev  
-            if not next or not next.next:
-                prev.next = next; 
-                break; 
-            prev.next = next.next;  
-            prev = next; 
-            curr = prev.next
-    return node
+  
+    remaing = head.next.next
+  
+    newhead = head.next
+  
+    head.next.next = head 
+  
+    head.next = swap_nodes_by_links(remaing) 
+  
+    return newhead
 
 n = ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5, ListNode(6))))))
-h = swap_nodes(n)
+h = swap_nodes_by_links(n)
 while h:
     print(h.val)
     h=h.next
